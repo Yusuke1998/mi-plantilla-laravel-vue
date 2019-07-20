dcd7b6c8dca25ecefe4603dff62882ce968bda2f
PrzJhonny <jhonnyjose1998@gmail.com>
2019-07-20 13:07:52 -0400

    mi plantilla de laravel con vue, con paquetes utiles!

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header" v-text="title"></div>
+
+                    <div class="card-body">
+                        <p v-text="body"></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+                title:'Hola, soy un componente!',
+                body:'Aqui se coloca informacion o algo asi :v'
+            }
+        },
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
